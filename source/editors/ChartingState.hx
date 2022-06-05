8b1de8646ff1a3c0c30f7d3e7a0987f6c961fe3b
Joalor64 <rhgarcon@gmail.com>
2022-06-05 12:31:02 -0400

    remove player3???

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -215,7 +215,6 @@ class ChartingState extends MusicBeatState
 				splashSkin: 'noteSplashes',//idk it would crash if i didn't
 				player1: 'bf',
 				player2: 'dad',
-				player3: null,
 				gfVersion: 'gf',
 				speed: 1,
 				stage: 'stage',
@@ -2771,7 +2770,6 @@ class ChartingState extends MusicBeatState
 
 			player1: _song.player1,
 			player2: _song.player2,
-			player3: null,
 			gfVersion: _song.gfVersion,
 			stage: _song.stage,
 			validScore: false
